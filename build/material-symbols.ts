1ee40be4ec5be46edca2f78b365320f45f060345
Gocti <Gocti>
2025-08-13 16:26:36 +0200

    Fix another import

--- build/material-symbols.ts
+++ build/material-symbols.ts
@@ -1,4 +1,4 @@
-import { fromCache, toCache } from './cache';
+import { fromCache, toCache } from './cache.ts';
 
 const BROWSER_USER_AGENT =
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
