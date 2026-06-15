3d339d036b4596ee27c5a270dd5e1efebc642abe
Gocti <Gocti>
2026-06-15 21:28:52 +0200

    Refactor how Harlowe internals are exposed

--- src/devtools-panel/api/expose-harlowe-internals.ts
+++ src/devtools-panel/api/expose-harlowe-internals.ts
@@ -11,13 +11,13 @@ import {
 import { executeCode } from './remote-execute';
 
 export async function exposeHarloweInternals(): Promise<void> {
-  if (!('chrome' in globalThis) || !('debugger' in globalThis.chrome)) {
-    const isDebug = await executeCode(() => 'REPL' in window);
-    if (isDebug) await executeCode(exposeUsingDebugREPL);
-    return;
-  }
+  // First attempt to expose internals by intercepting eval
+  if (await executeCode(exposeViaEvalIntercept)) return;
 
-  return exposeUsingChromeDevToolsProtocol();
+  if ('chrome' in globalThis && 'debugger' in globalThis.chrome) {
+    // Failing that, try expose via CDP.
+    return exposeUsingChromeDevToolsProtocol();
+  }
 }
 
 async function exposeUsingChromeDevToolsProtocol() {
@@ -63,44 +63,62 @@ async function exposeUsingChromeDevToolsProtocol() {
 }
 
 declare const REPL: (arg: unknown) => void;
+declare const Harlowe: any;
 
-function exposeUsingDebugREPL() {
+function exposeViaEvalIntercept() {
+  const exposeCode = `
+    window.__HarloweInternals = {
+      require,
+      engine: require('engine'),
+      state: require('state'),
+      passages: require('passages'),
+      macros: require('macros'),
+      renderer: require('renderer'),
+      section: require('section'),
+      utils: require('utils'),
+    };
+  `.replaceAll(/[\r\n\s]/g, '');
+
+  if (typeof Harlowe === 'object' && Harlowe && 'API_ACCESS' in Harlowe) {
+    Harlowe.API_ACCESS.SECTION.create().eval({ type: 'string', text: exposeCode });
+  }
+
+  if ('__HarloweInternals' in window) return true;
+
+  /*
+    Harlowe's section class is assigned an eval property which is basically a thin wrapper for eval,
+    However due to that wrapper code is evaluated inside the [[Scope]] of the Section.
+    This scope gives access to Harlowe's internal require.
+
+    Harlowe's REPL (debug method) creates a new section and calls eval on it; however...
+    REPL wraps the code `Macros.run("print", [sectoin,OURCODE])`, which breaks our code.
+
+    By setting a getter and setter for Object.prototype.eval, we can avoid our code being wrapped,
+    while still allowing us to execute in the desired [[Scope]].
+  */
   if ('REPL' in window && typeof window.REPL !== 'function') return false;
 
-  let originalEvalWrapper: (code: string) => void;
-
+  const mockCode = 'DUGGERMOCKCODE';
+  let baseEval: undefined | typeof eval = undefined;
   Object.defineProperty(Object.prototype, 'eval', {
+    configurable: true,
+    set(harloweEval) {
+      baseEval = harloweEval;
+    },
     get() {
-      return (code: string) => {
-        try {
-          originalEvalWrapper(`
-            window.__HarloweInternals = {
-              require,
-              engine: require('engine'),
-              state: require('state'),
-              passages: require('passages'),
-              macros: require('macros'),
-              renderer: require('renderer'),
-              section: require('section'),
-              utils: require('utils'),
-            };
-          `);
-        } catch (e) {
-          console.error('Harlowe prototype injection failed:', e);
-        } finally {
-          delete (Object.prototype as Record<string, unknown>).eval;
-        }
+      const capturedEval = baseEval;
+      if (!capturedEval) return;
 
-        return originalEvalWrapper(code);
+      return (code: string) => {
+        // If This is not out custom trigger, perform eval assigned to the object
+        if (!code.includes(mockCode)) return capturedEval(code);
+        // Else, delete our override and execute out expose-code
+        delete (Object.prototype as Record<string, unknown>).eval;
+        return capturedEval(exposeCode);
       };
     },
-    set(val) {
-      originalEvalWrapper = val;
-    },
-    configurable: true,
   });
 
-  REPL('1');
-
+  REPL(mockCode);
   return '__HarloweInternals' in window;
 }
